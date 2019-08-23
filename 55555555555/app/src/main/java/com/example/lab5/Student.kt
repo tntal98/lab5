56a0d48ff55c7b47ef9f82bc68d4884de2f108f0
tntal98 <tanaporn.so@kkumail.com>
2019-08-23 16:07:29 +0700

    lab5
    ธนาภรณ์ โสมรักษ์

---
+++ 55555555555/app/src/main/java/com/example/lab5/Student.kt
@@ -0,0 +1,8 @@
+package com.example.lab5
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Student(val id: String, val name: String, val age: Int) :Parcelable {
+}
